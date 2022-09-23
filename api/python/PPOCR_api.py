a5290bf47b2d9d11997dd47fcf7babdcebf9d271
hiroi-sora <2230247019@qq.com>
2022-09-23 09:29:49 +0800

    update PYTHON API

--- api/python/PPOCR_api.py
+++ api/python/PPOCR_api.py
@@ -4,7 +4,7 @@
 
 import os
 import subprocess  # 进程，管道
-from json import loads as jsonLoads
+from json import loads as jsonLoads, dumps as jsonDumps
 from sys import platform as sysPlatform  # popen静默模式
 
 
@@ -14,8 +14,7 @@ class PPOCR:
     def __init__(self, exePath: str, argument: dict = None):
         """初始化识别器。\n
         :exePath: 识别器`PaddleOCR_json.exe`的路径。\n
-        :argument: 启动参数，字典`{"键":值}`。参数说明见\n
-        `https://github.com/hiroi-sora/PaddleOCR-json#5-%E9%85%8D%E7%BD%AE%E4%BF%A1%E6%81%AF%E8%AF%B4%E6%98%8E`\n
+        :argument: 启动参数，字典`{"键":值}`。参数说明见 https://github.com/hiroi-sora/PaddleOCR-json
         """
         cwd = os.path.abspath(os.path.join(exePath, os.pardir))  # 获取exe父文件夹
         # 处理启动参数
@@ -25,8 +24,8 @@ class PPOCR:
                     exePath += f' --{key}:"{value}"'
                 else:
                     exePath += f' --{key}:{value}'
-        if 'use_system_pause' not in exePath:  # 强制禁用暂停
-            exePath += ' --use_system_pause=0'
+        if 'use_debug' not in exePath:  # 关闭debug模式
+            exePath += ' --use_debug=0'
         # 设置子进程启用静默模式，不显示控制台窗口
         startupinfo = None
         if 'win32' in str(sysPlatform).lower():
@@ -44,29 +43,31 @@ class PPOCR:
         while True:
             if not self.ret.poll() == None:  # 子进程已退出，初始化失败
                 raise Exception(f'OCR init fail.')
-            initStr = self.ret.stdout.readline().decode('gbk', errors='ignore')
+            initStr = self.ret.stdout.readline().decode('utf-8', errors='ignore')
             if 'OCR init completed.' in initStr:  # 初始化成功
                 break
-        print(f'初始化OCR成功，进程号为{self.ret.pid}')
 
-    def run(self, imgPath: str):
+    def run(self, imgPath: str, argument: dict = None):
         """对一张图片文字识别。\n
         :exePath: 图片路径。\n
+        :argument: (可选)热更新参数，字典`{"键":值}`。参数说明见 https://github.com/hiroi-sora/PaddleOCR-json\n
         :return:  {'code': 识别码, 'data': 内容列表或错误信息字符串}\n"""
         if not self.ret.poll() == None:
             return {'code': 400, 'data': f'子进程已崩溃。'}
-        wirteStr = imgPath if imgPath[-1] == '\n' else imgPath + '\n'
+        writeDict = {'image_dir': imgPath}
+        if argument:
+            writeDict.update(argument)
         # 输入路径
+        writeStr = jsonDumps(writeDict, ensure_ascii=True, indent=None)+'\n'
         try:
-            self.ret.stdin.write(wirteStr.encode('gbk'))
+            self.ret.stdin.write(writeStr.encode('utf-8'))
             self.ret.stdin.flush()
         except Exception as e:
             return {'code': 400, 'data': f'向识别器进程写入图片地址失败，疑似子进程已崩溃。{e}'}
-        if imgPath[-1] == '\n':
-            imgPath = imgPath[:-1]
         # 获取返回值
         try:
             getStr = self.ret.stdout.readline().decode('utf-8', errors='ignore')
+            # print(getStr)
         except Exception as e:
             return {'code': 401, 'data': f'读取识别器进程输出值失败，疑似传入了不存在或无法识别的图片 \"{imgPath}\" 。{e}'}
         try:
@@ -74,6 +75,12 @@ class PPOCR:
         except Exception as e:
             return {'code': 402, 'data': f'识别器输出值反序列化JSON失败，疑似传入了不存在或无法识别的图片 \"{imgPath}\" 。异常信息：{e}。原始内容：{getStr}'}
 
-    def __del__(self):
+    def runClipboard(self):
+        """立刻对剪贴板第一位的图片进行文字识别。"""
+        return self.run('clipboard')
+
+    def stop(self):
         self.ret.kill()  # 关闭子进程
-        print('关闭OCR子进程！')
+
+    def __del__(self):
+        self.stop()
