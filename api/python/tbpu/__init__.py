f8792cb53a5b9750a0befc2b9efa28a5a4b9bebc
hiroi-sora <2230247019@qq.com>
2022-11-03 20:03:40 +0800

    Python API: 添加段落合并模块

---
+++ api/python/tbpu/__init__.py
@@ -0,0 +1,66 @@
+# tbpu : text block processing unit
+# 文本块处理单元
+
+# OCR返回的结果中，一项包含文字、包围盒、置信度的元素，称为一个“文块” - text block 。
+# 文块不一定是完整的一句话或一个段落。反之，一般是零散的文字。
+# 一个OCR结果常由多个文块组成。
+# 文块处理器就是：将传入的多个文块进行处理，比如合并、排序、删除文块。
+
+
+# 概念解析：
+# 栏：一页纸可能有单栏、双栏、多栏，不同栏之间的文本块不会接壤。不同栏的文块是绝对不能合并的。
+# 段：一栏可能有多个段落，可能由行间距、起始空格等来区分。怎么划分段落、怎么合并，不同Tbpu有不同方案。
+# 行：一段内可能有多个行。应该尽量合并。
+# 块：文本块是OCR结果中的最小单位，一行可能被意外划分为多个块，这是不正常的，必须要合并。
+
+
+from .merge_line_h import TbpuLineH
+from .merge_line_h_m_left import TbpuLineHMultiLeft
+from .merge_line_h_m_paragraph import TbpuLineHMultiParagraph
+from .merge_line_h_m_fuzzy import TbpuLineHMultiFuzzy
+from .merge_line_v_lr import TbpuLineVlr
+from .merge_line_v_rl import TbpuLineVrl
+
+# 执行后处理可能破坏原列表textBlocks！
+
+
+def run_merge_line_h(textBlocks):
+    '''横排-单行'''
+    tbpu = TbpuLineH()
+    tb, debugMsg = tbpu.run(textBlocks, None)
+    return tb
+
+
+def run_merge_line_h_m_left(textBlocks):
+    '''横排-合并多行-左对齐'''
+    tbpu = TbpuLineHMultiLeft()
+    tb, debugMsg = tbpu.run(textBlocks, None)
+    return tb
+
+
+def run_merge_line_h_m_paragraph(textBlocks):
+    '''横排-合并多行-自然段'''
+    tbpu = TbpuLineHMultiParagraph()
+    tb, debugMsg = tbpu.run(textBlocks, None)
+    return tb
+
+
+def run_merge_line_h_m_fuzzy(textBlocks):
+    '''横排-合并多行-模糊匹配'''
+    tbpu = TbpuLineHMultiFuzzy()
+    tb, debugMsg = tbpu.run(textBlocks, None)
+    return tb
+
+
+def run_merge_line_v_lr(textBlocks):
+    '''竖排-从左到右-单行'''
+    tbpu = TbpuLineVlr()
+    tb, debugMsg = tbpu.run(textBlocks, None)
+    return tb
+
+
+def run_merge_line_v_rl(textBlocks):
+    '''竖排-从左到右-单行'''
+    tbpu = TbpuLineVrl()
+    tb, debugMsg = tbpu.run(textBlocks, None)
+    return tb
---
+++ api/python/tbpu/merge_line_v_rl.py
@@ -0,0 +1,20 @@
+# 文块处理：合并竖排单行-从右至左
+from .merge_line_v_lr import TbpuLineVlr
+
+from time import time
+
+
+class TbpuLineVrl(TbpuLineVlr):
+    def __init__(self):
+        self.tbpuName = '竖排-从右到左-单行'
+        self.rl = True  # T为从右到左，F为从左到右
+
+    def getInitInfo(self):
+        return f'文块后处理：[{self.tbpuName}]'
+
+    def run(self, textBlocks, imgInfo):
+        '''传入 文块组、图片信息。返回文块组、debug信息字符串。'''
+        timeIn = time()
+        resList, s = super().run(textBlocks, imgInfo)
+        # 返回新文块组和debug字符串
+        return resList, f'[{self.tbpuName}] 原{len(textBlocks)}块，合并后{len(resList)}块，耗时{time()-timeIn}s'
