609969058e845f8e594d44dc1e6efeca587e3d9f
jiangzian04121735 <117182011+jiangzian04121735@users.noreply.github.com>
2023-06-11 14:59:51 +0800

    Update demo.ps1
    demo

--- api/PowerShell/demo.ps1
+++ api/PowerShell/demo.ps1
@@ -1,21 +1,37 @@
-﻿Import-Module -Force ".\PPOCR_api.ps1"
+﻿Import-Module -Force ".\PPOCR_api.ps1"  #修改对应路径
 
 # 初始化识别器对象，传入 PaddleOCR_json.exe 的路径
-$exePath = Convert-Path ".\PaddleOCR-json_v.1.3.0_alpha.2\PaddleOCR-json.exe"
-$arg = ""
+$exePath = Convert-Path ".\PaddleOCR-json_v.1.3.0_alpha.2\PaddleOCR-json.exe"   #修改对应exe路径
+$arg = ""  #参数其实在API内没处理
 $ocr = [PPOCR]::new($exePath,$arg)
 
-# 识别图片，传入图片路径
 while (1) {
-    $imgPath = read-host "请输入图片路径，退出直接回车" 
-    if ($imgPath) {
-        $getObj = $ocr.run($imgPath)
-        Write-Host "图片识别完毕，状态码：$($getObj.'code') 结果：`n$($getObj.'data'|Out-String)`n"
-    }
-    else {
-        break
+    $选择 = Read-Host "1:图片路径`n2:剪贴板`n3:图片Base64`n其他按键退出"
+    switch($选择){
+        1{
+            # 识别图片
+            $imgPath = read-host "请输入图片路径"
+            if ($imgPath) {
+                $getObj = $ocr.run($imgPath)
+                Write-Host "图片识别完毕，状态码：$($getObj.'code') 结果：`n$($getObj.'data'|Out-String)`n"
+            }
+        }
+        2{
+            $getObj = $ocr.runClipboard()
+            Write-Host "图片识别完毕，状态码：$($getObj.'code') 结果：`n$($getObj.'data'|Out-String)`n"
+        }
+        3{
+            $imgBase64 = read-host "请输入图片BASE64" 
+            if ($imgBase64) {
+                $getObj = $ocr.runBase64($imgBase64)
+                Write-Host "图片识别完毕，状态码：$($getObj.'code') 结果：`n$($getObj.'data'|Out-String)`n"
+            }
+        }
+        Default
+        {
+            $ocr.stop()  # 结束子进程。
+            Write-Host "程序结束。"
+            Exit
+        }
     }
 }
-
-$ocr.stop()  # 结束子进程。
-Write-Host "程序结束。"
