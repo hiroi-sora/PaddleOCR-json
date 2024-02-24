d70e9d2a7c6796541f33873818dd3e99677da033
hiroi-sora <2230247019@qq.com>
2024-02-24 13:57:57 +0800

    fix: python api demo1 (#95)

--- api/python/demo1.py
+++ api/python/demo1.py
@@ -10,29 +10,30 @@ import os
 TestImagePath = f"{os.path.dirname(os.path.abspath(__file__))}\\test.jpg"
 
 # 初始化识别器对象，传入 PaddleOCR-json.exe 的路径。请改成你自己的路径
-ocr = GetOcrApi(r"D:\MyCode\CppCode\PaddleOCR-json\cpp\build\Release\PaddleOCR-json.exe")
-print(f'初始化OCR成功，进程号为{ocr.ret.pid}')
-print(f'\n测试图片路径：{TestImagePath}')
+ocr = GetOcrApi(r"D:\……\PaddleOCR-json.exe")
+print(f"初始化OCR成功，进程号为{ocr.ret.pid}")
+print(f"\n测试图片路径：{TestImagePath}")
 
 # 示例1：识别本地图片
 res = ocr.run(TestImagePath)
-print(f'\n示例1-图片路径识别结果（原始信息）：\n{res}')
-print(f'\n示例1-图片路径识别结果（格式化输出）：')
+print(f"\n示例1-图片路径识别结果（原始信息）：\n{res}")
+print(f"\n示例1-图片路径识别结果（格式化输出）：")
 ocr.printResult(res)
 
 # 示例2：识别剪贴板图片
 res = ocr.runClipboard()
 if res["code"] == 212:
-    print(f'\n示例2-当前剪贴板中没有图片。')
+    print(f"\n示例2-当前剪贴板中没有图片。")
 else:
-    print(f'\n示例2-剪贴板识别结果：')
+    print(f"\n示例2-剪贴板识别结果：")
     ocr.printResult(res)
 
 # 示例3：识别图片字节流
-with open(TestImagePath, 'rb') as f: # 获取图片字节流
-    imageBytes = f.read() # 实际使用中，可以联网下载或者截图获取字节流，直接送入OCR，无需保存到本地中转。
+with open(TestImagePath, "rb") as f:  # 获取图片字节流
+    # 实际使用中，可以联网下载或者截图获取字节流，直接送入OCR，无需保存到本地中转。
+    imageBytes = f.read()
 res = ocr.runBytes(imageBytes)
-print(f'\n示例3-字节流识别结果：')
+print(f"\n示例3-字节流识别结果：")
 ocr.printResult(res)
 
 # 示例4：识别 PIL Image 对象
@@ -40,15 +41,16 @@ try:
     from PIL import Image
     from io import BytesIO
 except Exception:
-    print("安装Pillow库后方可测试示例4。") 
+    print("安装Pillow库后方可测试示例4。")
     Image = None
 if Image:
     # 创建一个PIL Image对象
-    pilImage = Image.open(TestImagePath) 
+    pilImage = Image.open(TestImagePath)
     # Image 对象转为 字节流
-    buffered = BytesIO() 
+    buffered = BytesIO()
     pilImage.save(buffered, format="JPEG")
+    imageBytes = buffered.getvalue()
     # 送入OCR
     res = ocr.runBytes(imageBytes)
-    print(f'\n示例4-PIL Image 识别结果：')
+    print(f"\n示例4-PIL Image 识别结果：")
     ocr.printResult(res)
