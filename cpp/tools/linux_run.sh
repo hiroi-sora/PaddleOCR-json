38057ce06286eec2a4a4e3366e3f65862127036d
Gavin1937 <gyh1837771475gyh@gmail.com>
2024-06-07 19:24:46 -0700

    更新脚本 + 参数解释字符串

--- cpp/tools/linux_run.sh
+++ cpp/tools/linux_run.sh
@@ -6,16 +6,16 @@ if [ $# -eq 0 ]; then
     exit 0
 fi
 
-# 获取PaddleOCR-json路径
-EXE_LOCATION="$(pwd)/build/bin/PaddleOCR-json"
-
 # 获取当前脚本路径
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# 获取PaddleOCR-json路径
+EXE_LOCATION="$SCRIPT_DIR/../build/bin/PaddleOCR-json"
 
 # 所有PaddleOCR运行库都被复制到PaddleOCR-json的相同路径下了
 LIBS="$(dirname $EXE_LOCATION)"
 
-echo "LIBS: $LIBS"
+# echo "LIBS: $LIBS"
 
 # 运行PaddleOCR-json
 LD_LIBRARY_PATH="$LIBS" "$EXE_LOCATION" \
